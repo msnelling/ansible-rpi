51438861d0ea1bd2cd39cb5670be4891f25bac1c
Mark Snelling <mark@bakedbeans.com>
2018-06-14 22:49:26 +0100

    Fixed RPi initialisation.

--- init_rpi.sh
+++ init_rpi.sh
@@ -2,8 +2,9 @@
 
 INVENTORY=fresh_rpi_hosts.yml
 SITE=fresh_rpi_site.yml
+CONNECT_USER=pi
 VAULT_PASSWORD_FILE=.vault_pass
 
 echo "Initialising a new Raspberry Pi..."
 
-ansible-playbook --inventory="$INVENTORY" --ask-pass --vault-password-file="$VAULT_PASSWORD_FILE" "$SITE"
+ansible-playbook --inventory="$INVENTORY" --ask-pass --user="$CONNECT_USER" --vault-password-file="$VAULT_PASSWORD_FILE" "$SITE" -vvv
